99f51c0889394bf2901b49d4b5430fff1348d020
upgrade-square <rodneymike69@gmail.com>
2025-11-19 23:29:25 +0300

    mikrod3

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const PORT = process.env.PORT || 3000;
 // --- Middleware ---
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 
 // --- Load MikrodTech Knowledge Base ---
 let knowledgeData = {};
@@ -273,6 +274,7 @@ app.use((req, res, next) => {
 app.get("/api/visits", (req, res) => {
   res.json(visitData);
 });
+
 
 
 // ====================================================
